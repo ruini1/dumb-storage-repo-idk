0a6b237ffd12a08b685aea520e52daa7ed6acca0
ruini1 <89319877+ruini1@users.noreply.github.com>
2023-09-24 13:53:23 -0400

    added randomstring & randomhttp

--- main.lua
+++ main.lua
@@ -12,3 +12,24 @@ not for trade, dont save in your inventory.
 ------------------
 
 local script = game:GetObjects("rbxassetid://14873885392")[1]
+
+function RandomString(Length)
+	local Thread = ""
+	if type(Length) ~= "number" then
+		assert(Length, "'Length' must be a number.")
+		return
+	elseif type(Thread) ~= "string" then
+		assert(Thread, "'Thread' must be a string.")
+		return
+	end
+	for i = 1, Length do
+		Thread = Thread.. utf8.char(math.random(0, 10000))
+	end
+	return Thread
+end
+
+function RandomHTTP()
+	return game:GetService("HttpService"):GenerateGUID(true)
+end
+
+Auth = RandomHTTP()
