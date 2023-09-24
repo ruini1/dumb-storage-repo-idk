956b950e79959989111510770855b2c5215d22ec
ruini1 <89319877+ruini1@users.noreply.github.com>
2023-09-24 13:51:58 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+local LoadLibrary = game:GetObjects("rbxassetid://8299466510")[1]
+
+--[[ 
+
+not for trade, dont save in your inventory.
+
+]]--
+
+
+------------------
+--< MAIN >--
+------------------
+
+local script = game:GetObjects("rbxassetid://14873885392")[1]
